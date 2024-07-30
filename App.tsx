269c6dfcf21b51521a2028586f896e3da13510c3
Chris Jnr <junioaristotle@gmail.com>
2024-07-30 15:55:25 +0000

    Install Expo Router and setup the Tab navigation structure>

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Unmatched } from 'expo-router';
+export default Unmatched;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,41 @@
+import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export default function TabLayout() {
+    return (
+    <Tabs screenOptions={{ tabBarActiveTintColor : 'Blue', tabBarShowLabel:false}}>
+        <Tabs.Screen
+        name="index"
+        options={{
+        headerTitle: 'For you' ,
+         tabBarIcon : ({color}) =>(
+            <FontAwesome name="home" size={26} color = {color }/>
+         ) 
+       ,
+     }} 
+     />
+        <Tabs.Screen
+        name="new"
+        options={{
+        headerTitle: 'Create Post' ,
+         tabBarIcon : ({color}) =>(
+            <FontAwesome name="plus-square-o" size={26} color = {color }/>
+         ) 
+       ,
+     }} 
+     />
+        <Tabs.Screen
+        name="Profile"
+        options={{
+        headerTitle: 'Profile' ,
+         tabBarIcon : ({color}) =>(
+            <FontAwesome name="user" size={26} color = {color }/>
+         ) 
+       ,
+     }} 
+     />
+
+
+    </Tabs>
+    );
+}
